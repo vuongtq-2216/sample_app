010bf5db196f25f74d0b51c7686999c79f9501b2
vuongtq-2116 <thai.quoc.vuong@sun-asterisk.com>
2020-05-06 11:23:05 +0700

    Chapter 3: add i18n

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
