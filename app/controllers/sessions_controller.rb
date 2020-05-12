b1279dfd74c9e40c723a7c090c53984a655cfd05
vuongtq-2116 <thai.quoc.vuong@sun-asterisk.com>
2020-05-12 07:54:29 +0700

    chapter11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if user&.authenticate params[:session][:password]
       if user.activated?
         log_in user
-        params[:session][:remember_me] == Settings.users.remember ? remember(user) : forget(user)
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
         redirect_back_or user
       else
         flash[:warning] = t ".not_activated"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   ATTR_PARAMS = %i(name email password password_confirmation).freeze
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   self.per_page = Settings.users.per_page
   before_save{email.downcase!}
   before_create :create_activation_digest
@@ -44,6 +44,20 @@ class User < ApplicationRecord
 
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns reset_digest: User.digest(reset_token),
+                  reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.users.expire_time_reset_pass.hours.ago
   end
 
   private
